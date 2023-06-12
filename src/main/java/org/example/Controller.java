99fa42b50cfe61075d5726e979cc66012b3e6706
spring333 <spring3310@naver.com>
2023-06-12 11:45:59 +0900

    initial

---
+++ src/main/java/org/example/Controller.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application!";
+    }
+}
